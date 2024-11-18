4cbc6eee53a4d112a4173f6202eb08bca77b89d0
Jarred Parr <jparr721@gmail.com>
2024-11-18 05:29:25 -0700

    Fork Networking: WIP ripping out components

--- timeboost-networking/src/lib.rs
+++ timeboost-networking/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// Copyright (c) 2021-2024 Espresso Systems (espressosys.com)
+// This file is part of the HotShot repository.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// You should have received a copy of the MIT License
+// along with the HotShot repository. If not, see <https://mit-license.org/>.
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+//! Library for p2p communication
+
+/// Network logic
+pub mod network;
+
+/// symbols needed to implement a networking instance over libp2p-netorking
+pub mod reexport {
+    pub use libp2p::{request_response::ResponseChannel, Multiaddr};
+    pub use libp2p_identity::PeerId;
 }
