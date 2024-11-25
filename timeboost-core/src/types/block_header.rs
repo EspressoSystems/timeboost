bbf1675338bc372c567b459b9c81ff99494d47cc
Jarred Parr <jparr721@gmail.com>
2024-11-25 11:23:47 -0700

    Init Timeboost: Revert block header size

--- timeboost-core/src/types/block_header.rs
+++ timeboost-core/src/types/block_header.rs
@@ -3,11 +3,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub struct BlockHeader {}
-impl BlockHeader {
-    pub fn size_bytes(&self) -> usize {
-        0
-    }
-}
 
 impl Committable for BlockHeader {
     fn commit(&self) -> committable::Commitment<Self> {
