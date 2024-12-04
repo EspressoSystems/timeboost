d5e6b14f69659900008c4c2c58629abbfee673cb
Toralf Wittner <tw@dtex.org>
2024-12-04 16:42:44 +0100

    Expand (and ignore) RBC tests.

--- tests/src/tests/network/rbc.rs
+++ tests/src/tests/network/rbc.rs
@@ -6,9 +6,11 @@ use sailfish::rbc::Rbc;
 use timeboost_core::logging::init_logging;
 use timeboost_core::types::committee::StaticCommittee;
 use timeboost_core::types::event::SailfishEventType;
-use timeboost_core::types::Keypair;
+use timeboost_core::types::{Keypair, NodeId, PublicKey};
 
 use crate::rbc::TurmoilComm;
+
+type Peers<const N: usize> = [(PublicKey, (&'static str, u16)); N];
 
 fn fresh_keys(n: u64) -> (Vec<Keypair>, StaticCommittee) {
     let ks: Vec<Keypair> = (0..n).map(Keypair::zero).collect();
@@ -16,8 +18,45 @@ fn fresh_keys(n: u64) -> (Vec<Keypair>, StaticCommittee) {
     (ks, co)
 }
 
+/// Adds a sailfish host to the simulation.
+///
+/// The host consists of `Coordinator` and `Consensus` with
+/// `Rbc<TurmoilComm>` as its network communication layer.
+fn mk_host<T, const N: usize>(
+    id: T,
+    name: &str,
+    sim: &mut turmoil::Sim,
+    k: Keypair,
+    c: StaticCommittee,
+    addr: &'static str,
+    peers: Peers<N>,
+) where
+    T: Into<NodeId>,
+{
+    let id = id.into();
+    sim.host(name, move || {
+        let k = k.clone();
+        let c = c.clone();
+        async move {
+            let comm = TurmoilComm::create(addr, peers).await?;
+            let rbc = Rbc::new(comm, k.clone(), c.clone());
+            let cons = Consensus::new(id, k, c);
+            let mut coor = Coordinator::new(id, rbc, cons);
+            let mut actions = coor.start().await?;
+            loop {
+                for a in actions {
+                    coor.execute(a).await?;
+                }
+                actions = coor.next().await?
+            }
+        }
+    });
+}
+
+#[ignore]
 #[test]
-fn smoke() {
+#[rustfmt::skip]
+fn small_committee() {
     init_logging();
 
     let mut sim = turmoil::Builder::new()
@@ -25,7 +64,7 @@ fn smoke() {
         .max_message_latency(Duration::from_secs(7))
         .enable_random_order()
         .fail_rate(0.5)
-        .repair_rate(0.75)
+        .repair_rate(0.6)
         .simulation_duration(Duration::from_secs(500))
         .build();
 
@@ -37,56 +76,73 @@ fn smoke() {
         (*ks[2].public_key(), ("C", 9002)),
     ];
 
-    let key_a = ks[0].clone();
-    let committee_a = committee.clone();
+    mk_host(1, "A", &mut sim, ks[0].clone(), committee.clone(), "0.0.0.0:9000", peers);
+    mk_host(2, "B", &mut sim, ks[1].clone(), committee.clone(), "0.0.0.0:9001", peers);
 
-    sim.host("A", move || {
-        let key_a = key_a.clone();
-        let committee_a = committee_a.clone();
-        async move {
-            let comm = TurmoilComm::create("0.0.0.0:9000", peers).await?;
-            let rbc = Rbc::new(comm, key_a.clone(), committee_a.clone());
-            let cons = Consensus::new(1, key_a, committee_a);
-            let mut coor = Coordinator::new(1, rbc, cons);
-            let mut actions = coor.start().await?;
-            loop {
-                for a in actions {
-                    coor.execute(a).await?;
-                }
-                actions = coor.next().await?
-            }
-        }
-    });
-
-    let key_b = ks[1].clone();
-    let committee_b = committee.clone();
-
-    sim.host("B", move || {
-        let key_b = key_b.clone();
-        let committee_b = committee_b.clone();
-        async move {
-            let comm = TurmoilComm::create("0.0.0.0:9001", peers).await?;
-            let rbc = Rbc::new(comm, key_b.clone(), committee_b.clone());
-            let cons = Consensus::new(2, key_b, committee_b);
-            let mut coor = Coordinator::new(2, rbc, cons);
-            let mut actions = coor.start().await?;
-            loop {
-                for a in actions {
-                    coor.execute(a).await?;
-                }
-                actions = coor.next().await?
-            }
-        }
-    });
-
-    let key_c = ks[2].clone();
-    let committee_c = committee.clone();
+    let k = ks[2].clone();
+    let c = committee.clone();
 
     sim.client("C", async move {
         let comm = TurmoilComm::create("0.0.0.0:9002", peers).await?;
-        let rbc = Rbc::new(comm, key_c.clone(), committee_c.clone());
-        let cons = Consensus::new(3, key_c, committee_c);
+        let rbc = Rbc::new(comm, k.clone(), c.clone());
+        let cons = Consensus::new(3, k, c);
         let mut coor = Coordinator::new(3, rbc, cons);
+        let mut actions = coor.start().await?;
+        loop {
+            for a in actions {
+                if let Some(event) = coor.execute(a).await? {
+                    if let SailfishEventType::Committed { round, .. } = event.event {
+                        if round >= 3.into() {
+                            return Ok(());
+                        }
+                    }
+                }
+            }
+            actions = coor.next().await?
+        }
+    });
+
+    sim.run().unwrap()
+}
+
+#[ignore]
+#[test]
+#[rustfmt::skip]
+fn medium_committee() {
+    init_logging();
+
+    let mut sim = turmoil::Builder::new()
+        .min_message_latency(Duration::from_millis(10))
+        .max_message_latency(Duration::from_secs(7))
+        .enable_random_order()
+        .fail_rate(0.5)
+        .repair_rate(0.6)
+        .simulation_duration(Duration::from_secs(500))
+        .build();
+
+    let (ks, committee) = fresh_keys(5);
+
+    let peers = [
+        (*ks[0].public_key(), ("A", 9000)),
+        (*ks[1].public_key(), ("B", 9001)),
+        (*ks[2].public_key(), ("C", 9002)),
+        (*ks[3].public_key(), ("D", 9003)),
+        (*ks[4].public_key(), ("E", 9004)),
+    ];
+
+    mk_host(1, "A", &mut sim, ks[0].clone(), committee.clone(), "0.0.0.0:9000", peers);
+    mk_host(2, "B", &mut sim, ks[1].clone(), committee.clone(), "0.0.0.0:9001", peers);
+    mk_host(3, "C", &mut sim, ks[2].clone(), committee.clone(), "0.0.0.0:9002", peers);
+    mk_host(4, "D", &mut sim, ks[3].clone(), committee.clone(), "0.0.0.0:9003", peers);
+
+    let k = ks[4].clone();
+    let c = committee.clone();
+
+    sim.client("E", async move {
+        let comm = TurmoilComm::create("0.0.0.0:9004", peers).await?;
+        let rbc = Rbc::new(comm, k.clone(), c.clone());
+        let cons = Consensus::new(5, k, c);
+        let mut coor = Coordinator::new(5, rbc, cons);
         let mut actions = coor.start().await?;
         loop {
             for a in actions {
