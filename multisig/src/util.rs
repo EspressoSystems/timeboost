3bc6a377d7eabcf2a4a09240fd89e22f99b19639
Toralf Wittner <tw@dtex.org>
2024-12-10 13:06:28 +0100

    Reject small-order public keys.

--- multisig/src/util.rs
+++ multisig/src/util.rs
@@ -1,7 +1,7 @@
 use std::ops::Deref;
 
 use ed25519_compact as ed25519;
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use serde_bytes::ByteArray;
 
 pub(crate) fn encode<S, T, const N: usize>(d: &T, s: S) -> Result<S::Ok, S::Error>
@@ -16,8 +16,11 @@ pub(crate) fn decode_pk<'de, D>(d: D) -> Result<ed25519::PublicKey, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let a = ByteArray::deserialize(d)?;
-    Ok(ed25519::PublicKey::new(a.into_array()))
+    let a = ByteArray::deserialize(d)?.into_array();
+    if super::SMALL_ORDER_KEYS.contains(&&a) {
+        return Err(de::Error::custom("small order public key detected"));
+    }
+    Ok(ed25519::PublicKey::new(a))
 }
 
 pub(crate) fn decode_sk<'de, D>(d: D) -> Result<ed25519::SecretKey, D::Error>
