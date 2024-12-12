28a1891c9d8b6eaa05a75d505d310361c998ccae
Jarred Parr <jparr721@gmail.com>
2024-12-12 11:08:23 -0700

    block builder

--- timeboost/src/sequencer/phase/block_builder.rs
+++ timeboost/src/sequencer/phase/block_builder.rs
@@ -3,7 +3,9 @@ use timeboost_core::types::{
     block::timeboost::TimeboostBlock, time::Timestamp, transaction::Transaction,
 };
 
+pub mod canonical;
 pub mod noop;
+
 pub trait BlockBuilder {
     fn build(&self, ordered_transactions: (Timestamp, Vec<Transaction>)) -> Result<TimeboostBlock>;
 }
