240d71651b4faf369f45e0d392406c7b170493e9
Luke Iannucci <luke.iannucci@outlook.com>
2025-01-20 20:33:21 -0500

    more benchmarks

--- timeboost-networking/benches/network_bytes.rs
+++ timeboost-networking/benches/network_bytes.rs
@@ -1,9 +1,9 @@
 use bytes::Bytes;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn bench_clone_bytes(c: &mut Criterion) {
+fn bench_clone_single_bytes(c: &mut Criterion) {
     let data = Bytes::from(vec![0u8; 2_000]); // 2KB
-    c.bench_function("clone_bytes_crate", |b| {
+    c.bench_function("clone_single_bytes", |b| {
         b.iter(|| {
             let cloned = black_box(data.clone());
             black_box(cloned);
@@ -11,9 +11,9 @@ fn bench_clone_bytes(c: &mut Criterion) {
     });
 }
 
-fn bench_clone_vec(c: &mut Criterion) {
+fn bench_clone_single_vec_u8(c: &mut Criterion) {
     let data = vec![0u8; 2_000]; // 2KB
-    c.bench_function("clone_vec_bytes", |b| {
+    c.bench_function("clone_single_vec_u8", |b| {
         b.iter(|| {
             let cloned = black_box(data.clone());
             black_box(cloned);
@@ -21,5 +21,35 @@ fn bench_clone_vec(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_clone_bytes, bench_clone_vec);
+fn bench_clone_vec_of_bytes(c: &mut Criterion) {
+    let data: Vec<Bytes> = (0..64).map(|i| Bytes::from(vec![0; i * 1024])).collect();
+    c.bench_function("clone_vec_of_bytes", |b| {
+        b.iter(|| {
+            for d in data.iter() {
+                let cloned = black_box(d.clone());
+                black_box(&cloned);
+            }
+        })
+    });
+}
+
+fn bench_clone_vec_of_vec_u8(c: &mut Criterion) {
+    let data: Vec<Vec<u8>> = (0..64).map(|i| vec![0; i * 1024]).collect();
+    c.bench_function("clone_vec_of_vec_u8", |b| {
+        b.iter(|| {
+            for d in data.iter() {
+                let cloned = black_box(d.clone());
+                black_box(&cloned);
+            }
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_clone_single_bytes,
+    bench_clone_single_vec_u8,
+    bench_clone_vec_of_bytes,
+    bench_clone_vec_of_vec_u8
+);
 criterion_main!(benches);
