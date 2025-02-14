c9478e15bcf63a87c9051d428c534afb32a57d04
Toralf Wittner <tw@dtex.org>
2025-02-14 15:19:59 +0100

    Use unspecified epoch in network timestamps.
    Using `Instant` instead of `SystemTime` for transient duration
    measurements seems a little simpler. Also `Instant` is mononotic
    which is nice. IIRC earlier versions of this crate were also using
    `Instant` for ping duration measurements and I think it is the right
    approach.

--- timeboost-networking/src/time.rs
+++ timeboost-networking/src/time.rs
@@ -1,27 +1,48 @@
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::sync::LazyLock;
+use std::time::{Duration, Instant};
 
-/// Unix timestamp.
+/// An unspecified epoch for use with `Timestamp`.
+static EPOCH: LazyLock<Instant> = LazyLock::new(Instant::now);
+
+/// Time measured as duration in µs since an unspecified epoch.
 #[derive(Debug, Copy, Clone)]
-pub struct Timestamp(Duration);
+pub struct Timestamp(u64);
 
 impl Timestamp {
     pub fn now() -> Self {
-        let now = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap_or_default();
-        Self(now)
+        Self(Instant::now().saturating_duration_since(*EPOCH).as_micros() as u64)
     }
 
-    pub fn try_from_slice(b: &[u8]) -> Option<Self> {
-        let us = u64::from_be_bytes(b.try_into().ok()?);
-        Some(Self(Duration::from_micros(us)))
+    pub fn from_bytes(bytes: [u8; 8]) -> Self {
+        Self(u64::from_be_bytes(bytes))
     }
 
     pub fn to_bytes(self) -> [u8; 8] {
-        (self.0.as_micros() as u64).to_be_bytes()
+        self.0.to_be_bytes()
+    }
+
+    pub fn try_from_slice(b: &[u8]) -> Option<Self> {
+        let bytes = b.try_into().ok()?;
+        Some(Self::from_bytes(bytes))
     }
 
     pub fn diff(self, other: Self) -> Option<Duration> {
-        self.0.checked_sub(other.0)
+        self.0.checked_sub(other.0).map(Duration::from_micros)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Timestamp;
+    use tokio::time::{sleep, Duration};
+
+    #[tokio::test]
+    async fn duration() {
+        let d = Duration::from_millis(50);
+        let a = Timestamp::now();
+        sleep(d).await;
+        let b = Timestamp::now();
+        let x = b.diff(a).unwrap();
+        assert!(x - d < Duration::from_millis(5))
     }
 }
