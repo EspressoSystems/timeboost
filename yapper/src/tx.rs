aceb60100927cab609748c7d1acda16c184754f7
Jarred Parr <jparr721@gmail.com>
2025-04-03 17:36:03 -0600

    add tx

--- yapper/src/tx.rs
+++ yapper/src/tx.rs
@@ -1,20 +1,38 @@
+use reqwest::{Client, ClientBuilder, Url};
 use std::time::Duration;
-use url::Url;
+use timeboost_crypto::DecryptionScheme;
+use timeboost_crypto::traits::threshold_enc::ThresholdEncScheme;
 
 use anyhow::{Context, Result};
 use cliquenet::Address;
-use timeboost_utils::load_generation::tps_to_millis;
+use timeboost_utils::load_generation::{make_bundle, tps_to_millis};
 use tokio::time::interval;
-use tracing::warn;
+use tracing::{error, warn};
 
-pub async fn tx_sender(tps: u32, addr: Address) -> Result<()> {
+async fn send_transaction(
+    addr: &Address,
+    pubkey: &<DecryptionScheme as ThresholdEncScheme>::PublicKey,
+) -> Result<()> {
     let submision_url = Url::parse(&format!("http://{}/v0/submit", addr.to_string()))
         .context(format!("parsing {} into a url", addr.to_string()))?;
+    let bundle = make_bundle(pubkey)?;
+
+    Ok(())
+}
+
+pub async fn tx_sender(
+    tps: u32,
+    addr: Address,
+    pubkey: <DecryptionScheme as ThresholdEncScheme>::PublicKey,
+) -> Result<()> {
     let mut interval = interval(Duration::from_millis(tps_to_millis(tps)));
+    let client = ClientBuilder::new().timeout(Duration::from_secs(1)).build();
     loop {
         tokio::select! {
             _ = interval.tick() => {
-                // Send tx
+                if let Err(err) = send_transaction(&addr, &pubkey).await {
+                    error!(%err, "failed to send transaction");
+                }
             }
             _ = tokio::signal::ctrl_c() => {
                 warn!("sender for {addr} received shutdown signal");
