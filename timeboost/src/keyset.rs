5d8b4f950de6598f06a2ebc6e94332899d5c3384
Jarred Parr <jparr721@gmail.com>
2025-04-04 12:35:37 -0600

    Update timeboost/src/keyset.rs
    Co-authored-by: Toralf Wittner <tw@dtex.org>

--- timeboost/src/keyset.rs
+++ timeboost/src/keyset.rs
@@ -44,7 +44,7 @@ impl PublicDecInfo {
 }
 
 impl KeysetConfig {
-    pub fn read_keyset(path: &PathBuf) -> Result<Self> {
+    pub fn read_keyset(path: &Path) -> Result<Self> {
         ensure!(path.exists(), "File not found: {:?}", path);
         let data = fs::read_to_string(&path).context("Failed to read file")?;
         let keyset: KeysetConfig = from_str(&data).context("Failed to parse JSON")?;
