58e514cb17530a914db456c034b908e2c03f1d1c
Anders Konring <anders.konring@gmail.com>
2025-04-07 13:29:37 +0200

    reintroduce encrypted bundles in load generation

--- timeboost-utils/src/load_generation.rs
+++ timeboost-utils/src/load_generation.rs
@@ -1,19 +1,34 @@
 use arbitrary::{Arbitrary, Unstructured};
 use ark_std::rand::{self, Rng};
-use timeboost_crypto::{DecryptionScheme, traits::threshold_enc::ThresholdEncScheme};
+use bincode::error::EncodeError;
+use bytes::{BufMut, Bytes, BytesMut};
+use serde::Serialize;
+use timeboost_crypto::{
+    DecryptionScheme, KeysetId, Plaintext, traits::threshold_enc::ThresholdEncScheme,
+};
 use timeboost_types::{Address, Bundle, BundleVariant, PriorityBundle, SeqNo, Signer};
 
 type EncKey = <DecryptionScheme as ThresholdEncScheme>::PublicKey;
 
-pub fn make_bundle(_pubkey: &EncKey) -> anyhow::Result<BundleVariant> {
+pub fn make_bundle(pubkey: &EncKey) -> anyhow::Result<BundleVariant> {
     let mut rng = rand::thread_rng();
     let mut v = [0; 256];
     rng.fill(&mut v);
     let mut u = Unstructured::new(&v);
 
     let max_seqno = 10;
-    let bundle = Bundle::arbitrary(&mut u)?;
+    let mut bundle = Bundle::arbitrary(&mut u)?;
 
+    let kid = KeysetId::from(1);
+    if rng.gen_bool(0.5) {
+        // encrypt bundle
+        let data = bundle.data();
+        let plaintext = Plaintext::new(data.to_vec());
+        let ciphertext = DecryptionScheme::encrypt(&mut rng, &kid, pubkey, &plaintext)?;
+        let encoded = serialize(&ciphertext)?;
+        bundle.set_data(encoded.into());
+        bundle.set_kid(kid);
+    }
     if rng.gen_bool(0.5) {
         // priority
         let auction = Address::default();
@@ -31,4 +46,10 @@ pub fn make_bundle(_pubkey: &EncKey) -> anyhow::Result<BundleVariant> {
 /// Transactions per second to milliseconds is 1000 / TPS
 pub fn tps_to_millis<N: Into<u64>>(tps: N) -> u64 {
     1000 / tps.into()
+}
+
+fn serialize<T: Serialize>(d: &T) -> Result<Bytes, EncodeError> {
+    let mut b = BytesMut::new().writer();
+    bincode::serde::encode_into_std_write(d, &mut b, bincode::config::standard())?;
+    Ok(b.into_inner().freeze())
 }
