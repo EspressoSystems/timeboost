1249f7ecfd89760f41cb8cc33d5f7a8c0bb71730
Toralf Wittner <tw@dtex.org>
2025-06-09 18:50:41 +0200

    Remove unnecessary error case.

--- sailfish-rbc/src/error.rs
+++ sailfish-rbc/src/error.rs
@@ -1,5 +1,5 @@
 use cliquenet::overlay::{DataError, NetworkDown};
-use multisig::{CommitteeId, PublicKey};
+use multisig::CommitteeId;
 
 #[derive(Debug, thiserror::Error)]
 #[non_exhaustive]
@@ -21,9 +21,6 @@ pub enum RbcError {
 
     #[error("no committee for id {0}")]
     NoCommittee(CommitteeId),
-
-    #[error("missing address information for {1} in committee {0}")]
-    MissingAddrInfo(CommitteeId, PublicKey),
 
     #[error("rbc has shut down")]
     Shutdown,
