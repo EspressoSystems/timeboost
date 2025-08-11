ed1e843b83f50be4c998f2fabee82b94c1cd1abb
Anders Konring <anders.konring@gmail.com>
2025-08-11 10:04:34 +0200

    Add key stores and resharing accumulator/subset

--- timeboost-types/src/decryption.rs
+++ timeboost-types/src/decryption.rs
@@ -175,15 +175,122 @@ impl DecryptionKeyCell {
     }
 }
 
-/// A `Committee` with everyone's public key used in the DKG or key resharing for secure
-/// communication
+/// A small, non-empty collection of KeyStores.
+#[derive(Debug, Default, Clone)]
+#[allow(clippy::len_without_is_empty)]
+pub struct KeyStoreVec<const N: usize> {
+    vec: ArrayVec<KeyStore, N>,
+}
+
+impl<const N: usize> KeyStoreVec<N> {
+    /// Create a key store vector with the given entry.
+    pub fn new(k: KeyStore) -> Self {
+        const { assert!(N > 0) }
+        let mut this = Self {
+            vec: ArrayVec::new(),
+        };
+        this.add(k);
+        this
+    }
+
+    /// Check if an entry with the given committee ID exists.
+    pub fn contains(&self, id: CommitteeId) -> bool {
+        self.vec.iter().any(|k| k.committee().id() == id)
+    }
+
+    /// Get the index position for the given committee ID (if any).
+    ///
+    /// Key stores are ordered by recency, i.e. the higher the index,
+    /// the older the key store.
+    pub fn position(&self, id: CommitteeId) -> Option<usize> {
+        self.vec.iter().position(|k| k.committee().id() == id)
+    }
+
+    /// Get the key store corresponding to the given committee ID (if any).
+    pub fn get(&self, id: CommitteeId) -> Option<&KeyStore> {
+        self.vec.iter().find(|k| k.committee().id() == id)
+    }
+
+    /// Get the first (newest) key store.
+    pub fn first(&self) -> &KeyStore {
+        self.vec.first().expect("non-empty vector")
+    }
+
+    /// Get the last (oldest) key store.
+    pub fn last(&self) -> &KeyStore {
+        self.vec.last().expect("non-empty vector")
+    }
+
+    /// Get the number of key stores stored.
+    pub fn len(&self) -> usize {
+        self.vec.len()
+    }
+
+    /// Add a key store entry.
+    ///
+    /// If an entry with the given committee ID already exists, `add` is a NOOP.
+    /// This method will remove the oldest entry when at capacity.
+    pub fn add(&mut self, k: KeyStore) {
+        const { assert!(N > 0) }
+        if self.contains(k.committee().id()) {
+            return;
+        }
+        self.vec.truncate(N.saturating_sub(1));
+        self.vec.insert(0, k);
+    }
+
+    /// Like `add`, but moves `self`.
+    pub fn with(mut self, k: KeyStore) -> Self {
+        self.add(k);
+        self
+    }
+
+    /// Get an iterator over all key stores.
+    pub fn iter(&self) -> impl Iterator<Item = &KeyStore> {
+        self.vec.iter()
+    }
+
+    /// Check that the provided evidence is valid for the key stores.
+    pub fn is_valid(&self, r: RoundNumber, evidence: &Evidence) -> bool {
+        match evidence {
+            Evidence::Genesis => r.is_genesis(),
+            Evidence::Regular(x) => {
+                let Some(key_store) = self.get(x.data().committee()) else {
+                    return false;
+                };
+                evidence.round() + 1 == r && x.is_valid_par(key_store.committee())
+            }
+            Evidence::Timeout(x) => {
+                let Some(key_store) = self.get(x.data().round().committee()) else {
+                    return false;
+                };
+                evidence.round() + 1 == r && x.is_valid_par(key_store.committee())
+            }
+            Evidence::Handover(x) => {
+                let Some(key_store) = self.get(x.data().round().committee()) else {
+                    return false;
+                };
+                evidence.round() + 1 == r && x.is_valid_par(key_store.committee())
+            }
+        }
+    }
+}
+
+impl<const N: usize> From<KeyStore> for KeyStoreVec<N> {
+    fn from(k: KeyStore) -> Self {
+        const { assert!(N > 0) }
+        Self::new(k)
+    }
+}
+
+/// A `KeyStore` with committee information and public keys used in the DKG or key resharing
 #[derive(Debug, Clone)]
-pub struct DkgKeyStore {
+pub struct KeyStore {
     committee: Committee,
     keys: BTreeMap<KeyId, DkgEncKey>,
 }
 
-impl DkgKeyStore {
+impl KeyStore {
     pub fn new<I, T>(c: Committee, keys: I) -> Self
     where
         I: IntoIterator<Item = (T, DkgEncKey)>,
@@ -234,19 +341,17 @@ impl DkgKeyStore {
 /// Subset containing the aggregated contributions.
 #[derive(Debug, Clone)]
 pub struct DkgAccumulator {
-    store: DkgKeyStore,
+    store: KeyStore,
     threshold: usize,
     bundles: Vec<DkgBundle>,
-    completed: bool,
 }
 
 impl DkgAccumulator {
-    pub fn new(store: DkgKeyStore) -> Self {
+    pub fn new(store: KeyStore) -> Self {
         Self {
             threshold: store.committee().one_honest_threshold().get(),
             store,
             bundles: Vec::new(),
-            completed: false,
         }
     }
 
@@ -258,18 +363,12 @@ impl DkgAccumulator {
         &self.bundles
     }
 
-    pub fn completed(&self) -> bool {
-        self.completed
-    }
-
     pub fn is_empty(&self) -> bool {
         self.bundles.is_empty()
     }
 
     pub fn try_add(&mut self, bundle: DkgBundle) -> Result<(), VessError> {
-        if self.completed {
-            return Ok(());
-        }
+        // caller should ensure that no bundles are added after finalization
         let aad: &[u8; 3] = b"dkg";
         let committee = self.store.committee();
         let vess = Vess::new_fast();
@@ -284,13 +383,12 @@ impl DkgAccumulator {
         Ok(())
     }
 
-    pub fn try_finalize(&mut self) -> Option<Subset> {
-        if self.completed || self.bundles.len() >= self.threshold {
-            let subset = Subset::new(
+    pub fn try_finalize(&mut self) -> Option<DkgSubset> {
+        if self.bundles.len() >= self.threshold {
+            let subset = DkgSubset::new(
                 self.committee().id(),
                 self.bundles.clone().into_iter().collect(),
             );
-            self.completed = true;
             Some(subset)
         } else {
             None
@@ -300,12 +398,12 @@ impl DkgAccumulator {
 
 /// Represents a finalized subset of DKG bundles sufficient to combine.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
-pub struct Subset {
+pub struct DkgSubset {
     committe_id: CommitteeId,
     bundles: Vec<DkgBundle>,
 }
 
-impl Subset {
+impl DkgSubset {
     pub fn new(committe_id: CommitteeId, bundles: Vec<DkgBundle>) -> Self {
         Self {
             committe_id,
@@ -319,5 +417,105 @@ impl Subset {
 
     pub fn bundles(&self) -> &[DkgBundle] {
         &self.bundles
+    }
+}
+
+/// Accumulates resharing bundles (see `DkgAccumulator`).
+#[derive(Debug, Clone)]
+pub struct ResharingAccumulator {
+    store: KeyStore,
+    threshold: usize,
+    combkey: CombKey,
+    bundles: Vec<DkgBundle>,
+}
+
+impl ResharingAccumulator {
+    pub fn new(store: KeyStore, combkey: CombKey) -> Self {
+        Self {
+            threshold: store.committee().one_honest_threshold().get(),
+            store,
+            combkey,
+            bundles: Vec::new(),
+        }
+    }
+
+    pub fn committee(&self) -> &Committee {
+        &self.store.committee
+    }
+
+    pub fn bundles(&self) -> &[DkgBundle] {
+        &self.bundles
+    }
+
+    pub fn combkey(&self) -> &ThresholdCombKey {
+        &self.combkey
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.bundles.is_empty()
+    }
+
+    pub fn try_add(&mut self, bundle: DkgBundle) -> Result<(), VessError> {
+        // caller should ensure that no bundles are added after finalization
+        let Some(pub_share) = self.combkey.get_pub_share(bundle.origin().0.into()) else {
+            return Err(VessError::FailedVerification);
+        };
+
+        let aad: &[u8; 3] = b"dkg";
+        let committee = self.store.committee();
+        let vess = Vess::new_fast();
+        vess.verify_reshares(
+            committee,
+            self.store.sorted_keys(),
+            bundle.vess_ct(),
+            bundle.comm(),
+            aad,
+            *pub_share,
+        )?;
+        self.bundles.push(bundle);
+        Ok(())
+    }
+
+    pub fn try_finalize(&mut self) -> Option<ResharingSubset> {
+        if self.bundles.len() >= self.threshold {
+            let subset = ResharingSubset::new(
+                self.committee().id(),
+                self.bundles.clone().into_iter().collect(),
+                self.combkey().clone(),
+            );
+            Some(subset)
+        } else {
+            None
+        }
+    }
+}
+
+/// Represents a finalized subset of resharing bundles sufficient to combine.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct ResharingSubset {
+    committee_id: CommitteeId,
+    bundles: Vec<DkgBundle>,
+    combkey: CombKey,
+}
+
+impl ResharingSubset {
+    pub fn new(committee_id: CommitteeId, bundles: Vec<DkgBundle>, combkey: CombKey) -> Self {
+        Self {
+            committee_id,
+            bundles,
+            combkey,
+        }
+    }
+
+    pub fn committee_id(&self) -> &CommitteeId {
+        &self.committee_id
+    }
+
+    pub fn bundles(&self) -> &[DkgBundle] {
+        &self.bundles
+    }
+
+    pub fn combkey(&self) -> &CombKey {
+        &self.combkey
     }
 }
