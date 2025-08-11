6a1e8939d97b13932aa3f3bcca0ab1abef44a000
Alex Xiong <alex.xiong.tech@gmail.com>
2025-08-11 14:53:57 +0800

    use build.rs instead of forge bind

--- timeboost-contract/src/lib.rs
+++ timeboost-contract/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Timeboost Contract Bindings, Deployer and API bridges.
+//!
+//! This crate provides Rust bindings and API to interact with smart contracts,
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Include the generated contract bindings
+// The build script auto-detects contracts and generates bindings in src/bindings/
+pub mod bindings;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// We manually re-export the type here carefully due to alloy's lack of shared type:
+// tracking issue: https://github.com/foundry-rs/foundry/issues/10153
+pub use bindings::{erc1967proxy::ERC1967Proxy, keymanager::KeyManager};
