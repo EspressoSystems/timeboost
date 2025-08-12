ea07382fc0067aab42792572a5ecc2ef60b4f55c
alysiahuggins <hi@alysia.tech>
2025-08-12 00:35:28 -0400

    wip removed unused vars and added additional functions based on design feedback

--- contracts/src/KeyManager.sol
+++ contracts/src/KeyManager.sol
@@ -16,27 +16,35 @@ contract KeyManager is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     struct Committee {
         uint64 effectiveTimestamp;
         uint64 id;
-        CommitteeMember[] members;
+        CommitteeMember[] committeeMembers;
     }
 
-    event ScheduledCommittee(uint64 indexed id, Committee committee);
-    event CreatedEncryptionKeyset(uint64 indexed id, bytes thresholdEncKey);
+    event ScheduledCommittee(uint64 indexed id, uint64 effectiveTimestamp, uint256 membersCount, bytes32 membersHash);
+    event SetThresholdEncryptionKey(bytes thresholdEncKey);
     event ChangedManager(address indexed manager);
 
-    error NotOwnerOrManager();
-    error InvalidAddress();
+    error NotManager(address _sender);
+    error InvalidAddress(address _address);
+    error ThresholdEncryptionKeyAlreadySet();
+    error CommitteeIdDoesNotExist(uint64 _committeeId, uint64 _committeesLength);
+    error InvalidCommitteeMembers();
+    error InvalidEffectiveTimestamp(uint64 _effectiveTimestamp, uint64 _lastEffectiveTimestamp);
+    error NoCommitteeScheduled(uint64 _currentTimestamp, uint64 _lastEffectiveTimestamp);
+    error CommitteeIdOverflow();
 
-    bytes public thresholdEncKey;
-    uint64 public nextKeysetId;
-    uint64 public nextCommitteeId;
-    mapping(uint64 => Committee) public committees;
+    bytes public thresholdEncryptionKey;
+    Committee[] public committees;
     address public manager;
 
-    modifier onlyOwnerOrManager() {
-        if (msg.sender != owner() && msg.sender != manager) {
-            revert NotOwnerOrManager();
-        }
+    modifier onlyManager() {
+        _onlyManager();
         _;
+    }
+
+    function _onlyManager() internal view {
+        if (msg.sender != manager) {
+            revert NotManager(msg.sender);
+        }
     }
 
     constructor() {
@@ -45,7 +53,7 @@ contract KeyManager is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
     function initialize(address _manager) external initializer {
         if (_manager == address(0)) {
-            revert InvalidAddress();
+            revert InvalidAddress(_manager);
         }
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
@@ -54,32 +62,85 @@ contract KeyManager is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    function createEncryptionKeyset(bytes memory _thresholdEncKey) external onlyOwnerOrManager returns (uint64 keysetId) {
-        uint64 thisKeysetId = nextKeysetId;
-        thresholdEncKey = _thresholdEncKey;
-        emit CreatedEncryptionKeyset(nextKeysetId, _thresholdEncKey);
-        nextKeysetId++;
-        return thisKeysetId;
+    function setThresholdEncryptionKey(bytes calldata _thresholdEncKey) external onlyManager {
+        if (thresholdEncryptionKey.length > 0) {
+            revert ThresholdEncryptionKeyAlreadySet();
+        }
+        thresholdEncryptionKey = _thresholdEncKey;
+        emit SetThresholdEncryptionKey(_thresholdEncKey);
     }
 
-    function scheduleCommittee(uint64 _effectiveTimestamp, CommitteeMember[] calldata _members)
+    function setNextCommittee(uint64 _effectiveTimestamp, CommitteeMember[] calldata _members)
         external
-        onlyOwnerOrManager
-        returns (uint64)
+        onlyManager
+        returns (uint64 committeeId)
     {
-        uint64 thisCommitteeId = nextCommitteeId;
-        Committee memory newCommittee =
-            Committee({effectiveTimestamp: _effectiveTimestamp, id: nextCommitteeId, members: _members});
-        emit ScheduledCommittee(nextCommitteeId, newCommittee);
-        nextCommitteeId++;
-        return thisCommitteeId;
+        if (_members.length == 0) {
+            revert InvalidCommitteeMembers();
+        }
+
+        // ensure the effective timestamp is greater than the last effective timestamp
+        if (committees.length > 0) {
+            uint64 lastTimestamp = committees[committees.length - 1].effectiveTimestamp;
+            if (_effectiveTimestamp <= lastTimestamp) {
+                revert InvalidEffectiveTimestamp(_effectiveTimestamp, lastTimestamp);
+            }
+        }
+
+        if (committees.length > type(uint64).max) revert CommitteeIdOverflow();
+
+        committeeId = uint64(committees.length);
+        Committee storage newCommittee = committees.push();
+        newCommittee.effectiveTimestamp = _effectiveTimestamp;
+        newCommittee.id = committeeId;
+        newCommittee.committeeMembers = _members;
+
+        emit ScheduledCommittee(committeeId, _effectiveTimestamp, _members.length, keccak256(abi.encode(_members)));
+        return committeeId;
     }
 
     function setManager(address _manager) external onlyOwner {
         if (_manager == address(0)) {
-            revert InvalidAddress();
+            revert InvalidAddress(_manager);
         }
         manager = _manager;
         emit ChangedManager(_manager);
+    }
+
+    function getCommitteeById(uint64 id)
+        external
+        view
+        returns (uint64 effectiveTimestamp, CommitteeMember[] memory committeeMembers)
+    {
+        if (id >= committees.length) {
+            revert CommitteeIdDoesNotExist(id, uint64(committees.length));
+        }
+        return (committees[id].effectiveTimestamp, committees[id].committeeMembers);
+    }
+
+    function currentCommitteeId() external view returns (uint64 committeeId) {
+        if (committees.length == 0) {
+            revert CommitteeIdDoesNotExist(0, 0);
+        }
+
+        uint64 currentTimestamp = uint64(block.timestamp);
+        if (currentTimestamp < committees[0].effectiveTimestamp) {
+            revert NoCommitteeScheduled(currentTimestamp, committees[0].effectiveTimestamp);
+        }
+        if (currentTimestamp >= committees[committees.length - 1].effectiveTimestamp) {
+            return committees[committees.length - 1].id;
+        }
+
+        uint256 lo = 0;
+        uint256 hi = committees.length - 1;
+        while (lo < hi) {
+            uint256 mid = (lo + hi + 1) / 2;
+            if (committees[mid].effectiveTimestamp <= currentTimestamp) {
+                lo = mid;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return committees[lo].id;
     }
 }
--- contracts/test/KeyManager.t.sol
+++ contracts/test/KeyManager.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {KeyManager} from "../src/KeyManager.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
@@ -21,17 +21,16 @@ contract KeyManagerTest is Test {
         keyManagerProxy = KeyManager(address(proxy));
     }
 
-    function test_createEncryptionKeyset() public {
+    function test_setThresholdEncryptionKey() public {
         bytes memory thresholdEncKey = abi.encodePacked("1");
         vm.prank(manager);
         vm.expectEmit(true, true, true, true);
-        emit KeyManager.CreatedEncryptionKeyset(0, thresholdEncKey);
-        keyManagerProxy.createEncryptionKeyset(thresholdEncKey);
-        assertEq(keyManagerProxy.thresholdEncKey(), thresholdEncKey);
-        assertEq(keyManagerProxy.nextKeysetId(), 1);
+        emit KeyManager.SetThresholdEncryptionKey(thresholdEncKey);
+        keyManagerProxy.setThresholdEncryptionKey(thresholdEncKey);
+        assertEq(keyManagerProxy.thresholdEncryptionKey(), thresholdEncKey);
     }
 
-    function test_scheduleCommittee() public {
+    function test_setNextCommittee() public {
         KeyManager.CommitteeMember[] memory committeeMembers = new KeyManager.CommitteeMember[](1);
 
         bytes memory randomBytes = abi.encodePacked("1");
@@ -42,18 +41,31 @@ contract KeyManagerTest is Test {
             networkAddress: "0x0000000000000000000000000000000000000000"
         });
 
-        KeyManager.Committee memory committee = KeyManager.Committee({
-            effectiveTimestamp: uint64(block.timestamp),
-            id: keyManagerProxy.nextCommitteeId(),
-            members: committeeMembers
-        });
-
         vm.prank(manager);
         vm.expectEmit(true, true, true, true);
-        emit KeyManager.ScheduledCommittee(0, committee);
-        keyManagerProxy.scheduleCommittee(uint64(block.timestamp), committeeMembers);
+        emit KeyManager.ScheduledCommittee(
+            0, uint64(block.timestamp), committeeMembers.length, keccak256(abi.encode(committeeMembers))
+        );
+        keyManagerProxy.setNextCommittee(uint64(block.timestamp), committeeMembers);
 
-        assertEq(keyManagerProxy.nextCommitteeId(), 1);
+        // Test accessing the committee data
+        (uint64 effectiveTimestamp, KeyManager.CommitteeMember[] memory retrievedMembers) =
+            keyManagerProxy.getCommitteeById(0);
+        assertEq(effectiveTimestamp, uint64(block.timestamp));
+        assertEq(retrievedMembers.length, 1);
+        assertEq(retrievedMembers[0].pubKey, committeeMembers[0].pubKey);
+        assertEq(retrievedMembers[0].secureChannelKey, committeeMembers[0].secureChannelKey);
+        assertEq(retrievedMembers[0].dkgEncKey, committeeMembers[0].dkgEncKey);
+        assertEq(retrievedMembers[0].networkAddress, committeeMembers[0].networkAddress);
+
+        // Test accessing the current committee
+        uint64 currentCommitteeId = keyManagerProxy.currentCommitteeId();
+        assertEq(currentCommitteeId, 0);
+        (effectiveTimestamp, retrievedMembers) = keyManagerProxy.getCommitteeById(currentCommitteeId);
+        assertEq(effectiveTimestamp, uint64(block.timestamp));
+        assertEq(retrievedMembers.length, 1);
+        assertEq(retrievedMembers[0].pubKey, committeeMembers[0].pubKey);
+        assertEq(retrievedMembers[0].secureChannelKey, committeeMembers[0].secureChannelKey);
     }
 
     function test_setManager() public {
@@ -64,15 +76,30 @@ contract KeyManagerTest is Test {
         assertEq(keyManagerProxy.manager(), manager);
     }
 
-    function test_revertWhenNotOwnerOrManager_createEncryptionKeyset() public {
+    function test_revertWhenNotManager_setThresholdEncryptionKey() public {
         bytes memory thresholdEncKey = abi.encodePacked("1");
-        vm.expectRevert(abi.encodeWithSelector(KeyManager.NotOwnerOrManager.selector));
-        keyManagerProxy.createEncryptionKeyset(thresholdEncKey);
+        vm.expectRevert(abi.encodeWithSelector(KeyManager.NotManager.selector, address(this)));
+        keyManagerProxy.setThresholdEncryptionKey(thresholdEncKey);
     }
 
-    function test_revertWhenNotManager_scheduleCommittee() public {
-        vm.expectRevert(abi.encodeWithSelector(KeyManager.NotOwnerOrManager.selector));
-        keyManagerProxy.scheduleCommittee(uint64(block.timestamp), new KeyManager.CommitteeMember[](0));
+    function test_revertWhenThresholdEncryptionKeyAlreadySet_setThresholdEncryptionKey() public {
+        bytes memory thresholdEncKey = abi.encodePacked("1");
+        vm.startPrank(manager);
+        keyManagerProxy.setThresholdEncryptionKey(thresholdEncKey);
+        vm.expectRevert(abi.encodeWithSelector(KeyManager.ThresholdEncryptionKeyAlreadySet.selector));
+        keyManagerProxy.setThresholdEncryptionKey(thresholdEncKey);
+        vm.stopPrank();
+    }
+
+    function test_revertWhenNotManager_setNextCommittee() public {
+        vm.expectRevert(abi.encodeWithSelector(KeyManager.NotManager.selector, address(this)));
+        keyManagerProxy.setNextCommittee(uint64(block.timestamp), new KeyManager.CommitteeMember[](0));
+
+        // the owner should not be able to schedule the committee as it's not the manager
+        // the owner can become the manager by calling setManager
+        vm.prank(owner);
+        vm.expectRevert(abi.encodeWithSelector(KeyManager.NotManager.selector, owner));
+        keyManagerProxy.setNextCommittee(uint64(block.timestamp), new KeyManager.CommitteeMember[](0));
     }
 
     function test_revertWhenNotOwner_setManager() public {
@@ -83,7 +110,117 @@ contract KeyManagerTest is Test {
 
     function test_revertWhenInvalidAddress_setManager() public {
         vm.prank(owner);
-        vm.expectRevert(abi.encodeWithSelector(KeyManager.InvalidAddress.selector));
+        vm.expectRevert(abi.encodeWithSelector(KeyManager.InvalidAddress.selector, address(0)));
         keyManagerProxy.setManager(address(0));
+    }
+
+    // Tests for currentCommitteeId function
+    function test_revertWhenNoCommitteeScheduled_emptyCommittees() public {
+        vm.expectRevert(abi.encodeWithSelector(KeyManager.CommitteeIdDoesNotExist.selector, 0, 0));
+        keyManagerProxy.currentCommitteeId();
+    }
+
+    function test_currentCommitteeId_oneCommitteeScheduled_effectiveNow() public {
+        // Create a committee that's effective now
+        KeyManager.CommitteeMember[] memory committeeMembers = new KeyManager.CommitteeMember[](1);
+        bytes memory randomBytes = abi.encodePacked("1");
+        committeeMembers[0] = KeyManager.CommitteeMember({
+            pubKey: randomBytes,
+            secureChannelKey: randomBytes,
+            dkgEncKey: randomBytes,
+            networkAddress: "0x0000000000000000000000000000000000000000"
+        });
+
+        uint64 effectiveTimestamp = uint64(block.timestamp);
+        vm.prank(manager);
+        keyManagerProxy.setNextCommittee(effectiveTimestamp, committeeMembers);
+
+        uint64 currentCommitteeId = keyManagerProxy.currentCommitteeId();
+        assertEq(currentCommitteeId, 0);
+    }
+
+    function test_revertWhenNoCommitteeScheduled_currentCommitteeId() public {
+        // Create a committee that's effective in the future
+        KeyManager.CommitteeMember[] memory committeeMembers = new KeyManager.CommitteeMember[](1);
+        bytes memory randomBytes = abi.encodePacked("1");
+        committeeMembers[0] = KeyManager.CommitteeMember({
+            pubKey: randomBytes,
+            secureChannelKey: randomBytes,
+            dkgEncKey: randomBytes,
+            networkAddress: "0x0000000000000000000000000000000000000000"
+        });
+
+        uint64 effectiveTimestamp = uint64(block.timestamp + 100);
+        vm.prank(manager);
+        keyManagerProxy.setNextCommittee(effectiveTimestamp, committeeMembers);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                KeyManager.NoCommitteeScheduled.selector, uint64(block.timestamp), effectiveTimestamp
+            )
+        );
+        keyManagerProxy.currentCommitteeId();
+    }
+
+    function test_currentCommitteeId_singleCommittee_effectiveInThePast() public {
+        // Create a committee that was effective in the past
+        KeyManager.CommitteeMember[] memory committeeMembers = new KeyManager.CommitteeMember[](1);
+        bytes memory randomBytes = abi.encodePacked("1");
+        committeeMembers[0] = KeyManager.CommitteeMember({
+            pubKey: randomBytes,
+            secureChannelKey: randomBytes,
+            dkgEncKey: randomBytes,
+            networkAddress: "0x0000000000000000000000000000000000000000"
+        });
+
+        uint64 effectiveTimestamp = 100;
+        vm.prank(manager);
+        keyManagerProxy.setNextCommittee(effectiveTimestamp, committeeMembers);
+
+        vm.warp(101);
+        uint64 currentCommitteeId = keyManagerProxy.currentCommitteeId();
+        assertEq(currentCommitteeId, 0);
+    }
+
+    function test_currentCommitteeId_multipleCommittees() public {
+        // Create multiple committees with different timestamps
+        KeyManager.CommitteeMember[] memory committeeMembers = new KeyManager.CommitteeMember[](1);
+        bytes memory randomBytes = abi.encodePacked("1");
+        committeeMembers[0] = KeyManager.CommitteeMember({
+            pubKey: randomBytes,
+            secureChannelKey: randomBytes,
+            dkgEncKey: randomBytes,
+            networkAddress: "0x0000000000000000000000000000000000000000"
+        });
+
+        vm.startPrank(manager);
+
+        // Committee 0: effective now
+        uint64 timestamp0 = uint64(block.timestamp);
+        keyManagerProxy.setNextCommittee(timestamp0, committeeMembers);
+
+        // Committee 1: effective in 100 seconds
+        uint64 timestamp1 = uint64(block.timestamp + 100);
+        keyManagerProxy.setNextCommittee(timestamp1, committeeMembers);
+
+        // Committee 2: effective in 200 seconds
+        uint64 timestamp2 = uint64(block.timestamp + 200);
+        keyManagerProxy.setNextCommittee(timestamp2, committeeMembers);
+
+        vm.stopPrank();
+
+        // Test current committee (should be committee 0)
+        uint64 currentCommitteeId = keyManagerProxy.currentCommitteeId();
+        assertEq(currentCommitteeId, 0);
+
+        // Test at timestamp1 - only warp once to minimize gas
+        vm.warp(timestamp1);
+        currentCommitteeId = keyManagerProxy.currentCommitteeId();
+        assertEq(currentCommitteeId, 1);
+
+        // Test at timestamp2 - only warp once more
+        vm.warp(timestamp2);
+        currentCommitteeId = keyManagerProxy.currentCommitteeId();
+        assertEq(currentCommitteeId, 2);
     }
 }
