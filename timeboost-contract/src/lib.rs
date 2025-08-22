2d7807e8431567980b7f0854e2d1e4093f27a560
Alex Xiong <alex.xiong.tech@gmail.com>
2025-08-22 19:29:05 +0800

    switch from .env to .toml config file instead

--- timeboost-contract/src/lib.rs
+++ timeboost-contract/src/lib.rs
@@ -8,6 +8,7 @@ use alloy::{
     transports::http::reqwest::Url,
 };
 use anyhow::Result;
+use serde::Deserialize;
 
 // Include the generated contract bindings
 // The build script auto-detects contracts and generates bindings in src/bindings/
@@ -19,14 +20,28 @@ mod sol_types;
 use provider::{HttpProviderWithWallet, TestProviderWithWallet, build_provider};
 pub use sol_types::*;
 
+/// Config type for the key manager who has the permission to update the KeyManager contract
+/// See `test-configs/keymanager.toml` for an example
+#[derive(Deserialize)]
+struct KeyManagerConfig {
+    wallet: LocalWalletConfig,
+}
+
+#[derive(Deserialize)]
+struct LocalWalletConfig {
+    mnemonic: String,
+    index: u32,
+}
+
 /// Connect to a real blockchain, deploy the KeyManager contract, set the
 /// `TIMEBOOST_KEY_MANAGER_MNEMONIC` as the manager.
 /// Returns the (wallet provider, KeyManager address).
 pub async fn init_chain(chain: Url) -> Result<(HttpProviderWithWallet, Address)> {
-    dotenvy::dotenv()?;
+    let config =
+        toml::from_str::<KeyManagerConfig>(include_str!("../../test-configs/keymanager.toml"))?;
 
-    let mnemonic = dotenvy::var("TIMEBOOST_KEY_MANAGER_MNEMONIC")?;
-    let account_idx = dotenvy::var("TIMEBOOST_KEY_ACCOUNT_INDEX")?.parse::<u32>()?;
+    let mnemonic = config.wallet.mnemonic;
+    let account_idx = config.wallet.index;
     let provider = build_provider(mnemonic, account_idx, chain);
 
     let km_addr =
